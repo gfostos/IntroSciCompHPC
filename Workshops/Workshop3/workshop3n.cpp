5a4e5e4e566831d0832bbe6b25b73069503a50ab
Gabriel Ostos <gostos@unal.edu.co>
2025-05-14 10:17:19 -0500

    Workshop3

--- Workshops/Workshop3/workshop3n.cpp
+++ Workshops/Workshop3/workshop3n.cpp
@@ -7,16 +7,21 @@ int bar(int a, int b);
 double baz(double x);
 void print_array(const double data[], const int & size);
 
-int main (int argc, char **argv)
+int main(int argc, char **argv)
 {
     int ii = 0, jj = -1;
 
-    std::cout << "foo(ii, jj): " << foo(ii, jj) << std::endl;
-    std::cout << "foo(jj, ii): " << foo(jj, ii) << std::endl;
+    // Manejo de posibles divisiones por cero
+    if (jj != 0 && ii != 0)
+    {
+        foo(ii, jj);
+        foo(jj, ii);
+    }
+
     std::cout << "baz(25.9): " << baz(25.9) << std::endl;
 
     const int NX = 2, NY = 3, NZ = 4;
-    double *x = new double [NX]();
+    double *x = new double[NX];
     double y[NY] = {0.0}, z[NZ] = {0.0};
 
     print_array(x, NX);
@@ -24,8 +29,8 @@ int main (int argc, char **argv)
     print_array(z, NZ);
     std::cout << std::endl;
 
-    for (ii = 0; ii < NX; ++ii) {
-        x[ii] = ii;
+    for (int i = 0; i < NX; ++i) {
+        x[i] = i;
     }
 
     print_array(x, NX);
@@ -33,9 +38,12 @@ int main (int argc, char **argv)
     print_array(z, NZ);
     std::cout << std::endl;
 
-    for (jj = 0; jj < NY && jj < NX; ++jj) {
-        x[jj] = ii;
-        y[jj] = jj;
+    for (int j = 0; j < NY && j < NX; ++j) {
+        x[j] = NX + j;
+    }
+
+    for (int j = 0; j < NY; ++j) {
+        y[j] = j;
     }
 
     print_array(x, NX);
@@ -50,8 +58,8 @@ int main (int argc, char **argv)
 
 int foo(int a, int b)
 {
-    if (b == 0 || a == 0) return 0;
-    return a/b + b/bar(a, b) + b/a;
+    if (b == 0 || a == 0) return 0;  // evitar división por cero
+    return a / b + b / bar(a, b) + b / a;
 }
 
 int bar(int a, int b)
@@ -62,7 +70,8 @@ int bar(int a, int b)
 
 double baz(double x)
 {
-    if (x == 0) return 0;
+    if (x == 0.0) return 0.0;
+    double v = 1 - (x + 1);
     return std::sqrt(x);
 }
 
@@ -70,6 +79,7 @@ void print_array(const double data[], const int & size)
 {
     std::cout << std::endl;
     for (int ii = 0; ii < size; ++ii) {
-        std::cout << data[ii] << "  " ;
+        std::cout << data[ii] << "  ";
     }
+    std::cout << std::endl;
 }
