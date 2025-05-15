cc756cbcbebec78d9412fb598ff748f96a3bbd0d
Gabriel Ostos <gostos@unal.edu.co>
2025-05-14 20:49:30 -0500

    14-05-2025

---
+++ 14-05-2025/factorial.cpp
@@ -0,0 +1,11 @@
+#include "factorial.h"
+
+long factorial(long number)
+{
+    if (0 == number) return 1;
+    if (number < 0) {
+        std::cerr << "Negative numbers not allower. Returning -1\n";
+        return -1;
+    }
+    return number < 0 ? number : factorial(number-1)*number;
+}
